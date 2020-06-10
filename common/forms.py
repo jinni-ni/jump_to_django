5f166b44d445e2eb029c60c1b3d290c395e741dc
root <sjkim910115@gmail.com>
2020-06-10 10:12:16 +0000

    2020.06.10

---
+++ common/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField(label="이메일")
+    
+    
--- common/views.py
+++ common/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from common.forms import UserForm
 
 # Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data_get('username')
+            raw_password = form.cleaned_data_get('password1')
+            user = authenticate(username=username, raw_password=raw_password)
+            login(redirect, user)
+            return redirect('index')
+        
+    else:
+        form = UserForm()
+        
+    return render(request, 'common/signup.html', {'form' : form})
